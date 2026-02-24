7204a10ba7b79480fa1d86e22259f4cfb6054cce
Odong Sunday <sandieo.2020@gmail.com>
2026-02-24 10:16:49 +0300

    update admin user request

--- src/main/java/com/sms/gateway/admin/dto/CreateAdminUserRequest.java
+++ src/main/java/com/sms/gateway/admin/dto/CreateAdminUserRequest.java
@@ -1,10 +1,27 @@
 package com.sms.gateway.admin.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 
 public class CreateAdminUserRequest {
     @NotBlank
     private String username;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String department;
 
     @NotBlank
     private String password;
@@ -33,5 +50,45 @@ public class CreateAdminUserRequest {
 
     public void setEnabled(Boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
     }
 }
