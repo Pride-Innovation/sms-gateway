25c095e6dea3de26128b0b97bd2d42a60d673939
Odong Sunday <sandieo.2020@gmail.com>
2026-03-03 12:05:02 +0300

    update api client repo

--- src/main/java/com/sms/gateway/users/ApiClientRepository.java
+++ src/main/java/com/sms/gateway/users/ApiClientRepository.java
@@ -1,5 +1,7 @@
 package com.sms.gateway.users;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -7,4 +9,5 @@ import java.util.Optional;
 public interface ApiClientRepository extends JpaRepository<ApiClient, Long> {
     Optional<ApiClient> findByUsernameIgnoreCase(String username);
     boolean existsByUsernameIgnoreCase(String username);
+    Page<ApiClient> findByUsernameContainingIgnoreCase(String username, Pageable pageable);
 }
