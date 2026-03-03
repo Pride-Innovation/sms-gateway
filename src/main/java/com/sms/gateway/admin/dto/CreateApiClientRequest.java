f4dc6d94c52604553da5becac45f47c8f92ad282
Odong Sunday <sandieo.2020@gmail.com>
2026-03-03 16:00:49 +0300

    update dto to use lombok

--- src/main/java/com/sms/gateway/admin/dto/CreateApiClientRequest.java
+++ src/main/java/com/sms/gateway/admin/dto/CreateApiClientRequest.java
@@ -1,7 +1,11 @@
 package com.sms.gateway.admin.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class CreateApiClientRequest {
 
     @NotBlank
@@ -13,27 +17,4 @@ public class CreateApiClientRequest {
     @NotBlank
     private String description;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
