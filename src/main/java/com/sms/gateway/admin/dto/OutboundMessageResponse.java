adbfbcd9b5302dff3005f210a5459d1693b73ceb
Odong Sunday <sandieo.2020@gmail.com>
2026-03-06 15:51:00 +0300

    update OutboundMessageAdmin response

--- src/main/java/com/sms/gateway/admin/dto/OutboundMessageResponse.java
+++ src/main/java/com/sms/gateway/admin/dto/OutboundMessageResponse.java
@@ -6,6 +6,8 @@ import java.time.Instant;
 public record OutboundMessageResponse(
         String requestId,
         String phone,
+        Long apiClientId,
+        String apiClientName,
         Carrier carrier,
         String message,
         String senderId,
