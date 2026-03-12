0ccfd75fc5cf5f1d4d6bdaa68732c05e662aba29
OdongAlican <sandieo.2020@gmail.com>
2026-03-12 16:00:17 +0300

    update sms gateway

--- src/main/java/com/sms/gateway/config/SmsGatewayProperties.java
+++ src/main/java/com/sms/gateway/config/SmsGatewayProperties.java
@@ -10,6 +10,15 @@ public class SmsGatewayProperties {
     private int queueCapacity = 20000;
     private int workerThreads = 16;
     private int maxTextChars = 2000;
+    private int otpPriority = 100;
+    private int notificationPriority = 10;
+    private int otpDefaultTtlSeconds = 300;
+    private int maxRetryAttempts = 5;
+    private int retryBaseDelaySeconds = 5;
+    private int retryMaxDelaySeconds = 300;
+    private int workerIdleSleepMs = 100;
+    private int recoveryIntervalSeconds = 30;
+    private int staleProcessingTimeoutSeconds = 120;
 
     /**
      * Alphanumeric sender IDs are often limited by SMSCs (commonly 11).
