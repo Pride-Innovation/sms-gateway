2b12e334176ff53daabba9935f5e2adb3ce9ff94
OdongAlican <sandieo.2020@gmail.com>
2026-03-17 12:57:47 +0300

    add AccountLockedException

---
+++ src/main/java/com/sms/gateway/adminuser/AccountLockedException.java
@@ -0,0 +1,8 @@
+package com.sms.gateway.adminuser;
+
+public class AccountLockedException extends RuntimeException {
+
+    public AccountLockedException(String message) {
+        super(message);
+    }
+}
